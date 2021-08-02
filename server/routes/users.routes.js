958187ffd884a9c9e08e51fa084cb7d7fa73ada0
Michael Zimmerman <mikezne@gmail.com>
2021-08-02 16:48:14 -0500

    makes /signup route

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -3,6 +3,7 @@ const passport = require("passport");
 const router = express.Router();
 const { signup, login } = require("../models/users.model");
 const auth = require("../middleware/auth.middleware");
+const { validate } = require("uuid");
 
 router.get("/validation", auth, (req, res) => {
   return res.send({
@@ -15,4 +16,16 @@ router.get("/validation", auth, (req, res) => {
 router.get("/logout", (req, res) => {
   res.clearCookie("jwt");
   return res.send({ success: true, error: null, body: null });
+});
+
+router.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+  if (validate(username, password)) {
+    return signup(res, username, password);
+  }
+  return res.send({
+    success: false,
+    data: null,
+    error: "Invalid data provided by user",
+  });
 });
