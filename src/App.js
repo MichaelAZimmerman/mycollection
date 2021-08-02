7004857d7a7e1af48e12ea6dac00dd8f7f4172f0
Michael Zimmerman <mikezne@gmail.com>
2021-08-02 12:02:35 -0500

    imports some things I'll need to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import "./App.css";
+import {
+  NavLink,
+  Redirect,
+  Route,
+  Switch,
+  useLocation,
+} from "react-router-dom";
+import BrowseUsers from "./components/BrowseUsers";
+import Login from "./components/Login";
+import MyCollection from "./components/MyCollection";
+import MyWishlist from "./components/MyWishlist";
+import Search from "./components/Search";
+import Signup from "./components/Signup";
 
 function App() {
   return <div className="App"></div>;
