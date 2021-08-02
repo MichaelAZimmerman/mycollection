fa3d3407cdebf95b7e506ffc49c53cc29e8864f0
Michael Zimmerman <mikezne@gmail.com>
2021-08-02 16:20:41 -0500

    makes signup model

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -20,3 +20,28 @@ async function getByUserID(uuid) {
     return json;
   }
 }
+
+async function signup(res, username, password) {
+  let json = { data: null, success: false, error: null };
+  try {
+    const users = await query("SELECT * FROM users WHERE username = ?", [
+      username,
+    ]);
+    if (users.length !== 0) {
+      json.error = "Username already taken";
+    } else {
+      const hashed = await bcrypt.hash(password, 8);
+      const uuid = uuidv4();
+      await query(
+        "INSERT INTO users (password, username, uuid) VALUES (?,?,?)",
+        [hashed, username, uuid]
+      );
+      json = { ...json, success: true };
+    }
+  } catch (err) {
+    console.log(err);
+    json.error = "Something went wrong!";
+  } finally {
+    return res.send(json);
+  }
+}
