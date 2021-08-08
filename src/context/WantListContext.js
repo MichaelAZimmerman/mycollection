413a724f4d2ef8a2fe586815732fa89e56fd5371
Michael Zimmerman <mikezne@gmail.com>
2021-08-08 12:27:48 -0500

    creates want list context

---
+++ src/context/WantListContext.js
@@ -0,0 +1,38 @@
+import React, { createContext, useCallback, useState } from "react";
+import useFetch from "../hooks/useFetch";
+export const WantListContext = createContext(null);
+
+export function WantListProvider(props) {
+  const { APIcall: deleteCall } = useFetch("DELETE");
+  const { APIcall: addCall } = useFetch("POST");
+  const [wantList, setWantList] = useState([]);
+
+  const addWantList = useCallback(async (album) => {
+    const res = await addCall("/api/mywantlist/add", { album });
+    if (!res.success) {
+      return console.error(res.error);
+    }
+    setWantList((curr) => [...curr, res.data]);
+  }, []);
+
+  const removeWantList = useCallback(async (album_id) => {
+    const res = await deleteCall(`/api/mywantlist/delete/${album_id}`);
+    if (!res.success) {
+      return console.error(res.error);
+    }
+    setWantList((curr) => curr.filter((val) => val.album_id !== album_id));
+  }, []);
+
+  return (
+    <WantListContext.Provider
+      value={{
+        wantList,
+        setWantList,
+        addWantList,
+        removeWantList,
+      }}
+    >
+      {props.children}
+    </WantListContext.Provider>
+  );
+}
